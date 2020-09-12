226a8a1478a5ab58fd956f0dd0f80906f4584f63
Peter Miller <peteremiller@outlook.com>
2020-09-12 11:27:46 -0500

    Updating the sql code for deliverable 2 in mod 7.

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -60,42 +60,62 @@ SELECT title, from_date, to_date
 FROM titles
 
 --Joining employees and titles for new retirement titles table
-SELECT employees.emp_no, birth_date, first_name, last_name,
-	 titles.title, 
-	 titles.from_date, 
-	 titles.to_date
+SELECT e.emp_no, birth_date, first_name, last_name,
+	 t.title, 
+	 t.from_date, 
+	 t.to_date
 INTO retirement_titles
-FROM titles	
-INNER JOIN employees
-ON employees.emp_no = titles.emp_no
+FROM titles	as t
+INNER JOIN employees as e
+ON e.emp_no = t.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY employees.emp_no;
+ORDER BY e.emp_no;
 
 --SELECT * FROM retirement_titles
 --DROP TABLE retirement_titles
 
 --Use the Distinct ON statement to retrieve the first occurance of the emp_no
 --for each set of rows defined by the ON() clause.
-SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no,
-	retirement_titles.first_name,
-	retirement_titles.last_name,
-	retirement_titles.title
+SELECT DISTINCT ON (rts.emp_no) rts.emp_no,
+	rts.first_name,
+	rts.last_name,
+	rts.title
 INTO unique_titles
-FROM retirement_titles
-ORDER BY retirement_titles.emp_no ASC, retirement_titles.to_date DESC
+FROM retirement_titles as rts
+ORDER BY rts.emp_no ASC, rts.title DESC;
 
---Create a Unique Titles table using the INTO clause
-SELECT COUNT (unique_titles.title), unique_titles.title
+--Create a Unique Titles table using the INTO clause to get the number of employees who are
+--about to retire. Order the query by their most recent job desc.
+SELECT COUNT (ut.title), ut.title
 INTO retiring_titles
-FROM unique_titles
-GROUP BY unique_titles.title
-ORDER BY COUNT (unique_titles.title) DESC;
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT (ut.title) DESC;
 
-SELECT * FROM retiring_titles
+SELECT * FROM unique_titles
 
 --Create a table to hold the employees who are eligible to participate 
 -- in a mentorship program: Mentorship Eligibiliy.
-SELECT DISTINCT ON (employees.emp_on) employees.emp_no,
-	employees.first_name,
-	employees.last_name,
-	employees.birth_date,
+--Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+SELECT DISTINCT ON (e.emp_on) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+--Retrieve the from_date and to_date columns from the Department Employee table.
+	dept_emp.from_date,
+	dept_emp.to_date,
+--Retrieve the title column from the Titles table.
+	t.title
+INTO mentorship_eligibility
+FROM e, dept_emp, t
+--Use a DISTINCT ON statement to retrieve the first occurrence of the employee number 
+--for each set of rows defined by the ON () clause.
+INNER JOIN departments as d 
+ON (e.emp_no = dept_emp.emp_no)
+INNER JOIN 
+ON (e.emp_no = t.emp_no)
+--Filter the data on the to_date column to get current employees whose birth dates
+--are between January 1, 1965 and December 31, 1965.
+WHERE (to_date BETWEEN '1965-01-01' AND '1965-12-31')
+--Order the table by the employee number.
+ORDER BY e.emp_no
